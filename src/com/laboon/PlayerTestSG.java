aa01b5f35d04c609d620af7c337d33d67247fc6a
Shelley Goldberg <sag109@pitt.edu>
2015-02-13 16:25:23 -0500

    add some player tests

--- src/com/laboon/PlayerTestSG.java
+++ src/com/laboon/PlayerTestSG.java
@@ -1,6 +1,7 @@
 /*
  * hasAllItems()
  * drink()
+ * public Player(boolean sugar, boolean cream, boolean coffee)
  */
 package com.laboon;
 import static org.junit.Assert.*;
@@ -13,8 +14,25 @@ import org.mockito.*;
 public class PlayerTestSG {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNoItems() {
+		Player play= new Player(true, true, true);
+		assertTrue(play.hasAllItems());
 	}
-
+	
+	@Test
+	public void testNoSugar() {
+		Player play= new Player(false, true, true);
+		assertFalse(play.hasAllItems());
+	}
+	
+	@Test
+	public void testNoCream() {
+		Player play= new Player(true, false, true);
+		assertFalse(play.hasAllItems());
+	}
+	@Test
+	public void testNoCoffee() {
+		Player play= new Player(true, true, false);
+		assertFalse(play.hasAllItems());
+	}
 }
